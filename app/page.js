1aba01874e5488072e594375fc708df488176324
Monish14-tech <monishmani979@gmail.com>
2026-01-01 23:03:26 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Gym Website</h1>
+      <p>Welcome to my gym website 🚀</p>
+    </main>
+  );
+}
